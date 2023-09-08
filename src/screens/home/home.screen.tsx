2013c98372cc3dab071057fd9370ee0c51649dcb
Jose Carvajal <josecarvajal@joses-mbp.lan>
2023-09-08 10:47:56 -0700

    feat: permissions handler

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -1,17 +1,39 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import {BaseLayout} from '../../components/layouts';
 import {styles} from './home.styles';
 import Icon from 'react-native-vector-icons/Feather';
 import {colors} from '../../constants/foundations';
 import {useNavigation} from '@react-navigation/native';
+import {openSettings, request, PERMISSIONS} from 'react-native-permissions';
+import {isIOS} from '../../constants/config';
 
 Icon.loadFont();
 
 export const Home: FC = () => {
   const navigation = useNavigation();
+  const [isCameraPermissionGranted, setCameraPermissionGranted] =
+    useState(false);
+  useEffect(() => {
+    checkPermissions();
+  }, []);
+  const checkPermissions = async () => {
+    const cameraPermission = await requestCameraPermission();
+    setCameraPermissionGranted(cameraPermission === 'granted');
+  };
+
+  const requestCameraPermission = async () => {
+    return isIOS
+      ? request(PERMISSIONS.IOS.CAMERA)
+      : request(PERMISSIONS.ANDROID.CAMERA);
+  };
+
   const onOpenCamera = () => {
-    navigation.navigate('Camera');
+    if (isCameraPermissionGranted) {
+      navigation.navigate('Camera');
+    } else {
+      openSettings();
+    }
   };
   return (
     <BaseLayout title="Gallery">
